5e6b6d3040b2fb9061566fc5f222fd12c4f97b8d
Gabe Benjamin <gabekebenjamin@gmail.com>
2017-05-13 18:38:01 -0700

    in progress

---
+++ get_credentials.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+APPLICATION_NAME = 'Gmail Rosie'
+CLIENT_SECRET_FILE = 'client_secret.json'
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.dirname(os.path.realpath(__file__))
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-credentials.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+
+    # Try and get labels to see if everything is working
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    service.users().labels().list(userId='me').execute()
+
+
+if __name__ == '__main__':
+    main()
---
+++ rosie.py
@@ -0,0 +1,39 @@
+def handler(event, context):
+    """
+    The AWS Lambda handler that will be called
+
+    :param event: AWS Lambda uses this parameter to pass in event data to the
+    handler. This parameter is usually of the Python dict type. It can also be
+    list, str, int, float, or NoneType type.
+    :param context: AWS Lambda uses this parameter to provide runtime
+    information to your handler. This parameter is of the LambdaContext type.
+    """
+    pass
+service = None
+def getService():
+    credentials = get_credentials()
+
+    # Try and get labels to see if everything is working
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+
+def cleanGmail():
+    """
+
+    :return:
+    """
+
+labelIdMap = {}
+def getLabelIdsForLabels(labels):
+    # Get all the labels since you can only get them by ids
+    results = service.users().labels().list(userId='me').execute()
+    allUserLabels = results.get('labels', [])
+
+    for label in allUserLabels:
+        labelName = label.get('name', '')
+        if labelName in labels:
+            labelIdMap[labelName] = label.get('id', None)
+
+def getThreadsForLabels(labelIds):
+    service.users().threads().list(userId='me', labelIds=[labelIds]).execute()
